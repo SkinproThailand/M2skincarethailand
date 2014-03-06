2e229b16ad420434a159525c4db118465c6acb78
Akarapong Samranpong <akarapongs@tokenpay.com>
2014-03-06 17:25:59 +0700

    update woocommerce

--- wp-content/plugins/woocommerce/templates/loop/price.php
+++ wp-content/plugins/woocommerce/templates/loop/price.php
@@ -15,6 +15,3 @@ global $product;
 <?php if ( $price_html = $product->get_price_html() ) : ?>
 	<span class="price"><?php echo $price_html; ?></span>
 <?php endif; ?>
-
-<!-- lek facebook like button -->
-<div><iframe src="http://www.facebook.com/plugins/like.php?href=<?php the_permalink(); ?>&amp;layout=button_count&amp;show_faces=false&amp;width=450&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;locale=en_US" scrolling="no" frameborder="0" allowtransparency="true" style="border:none; overflow:hidden; width:auto; height:25px;"></iframe></div>
